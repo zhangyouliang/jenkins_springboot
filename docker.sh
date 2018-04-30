b07cf6689e7d9ea458444027483a05cb0b2e3f9c
YouliangZhang <1577121881@qq.com>
2018-04-30 17:08:08 +0800

    init

--- docker.sh
+++ docker.sh
@@ -1,7 +1,14 @@
 #!/usr/bin/env bash
 
-aliimage=10.0.0.44:5000/jenkins_springboot:`TZ=CST-8 date '+%Y%m%d-%H%M'`
-if [ "`docker images | awk '/^10.0.0.44:5000/ { print $3 }'`" ]; then docker rmi -f $(docker images | awk '/^10.0.0.44:5000/ { print $3 }' ); fi
+image_name=10.0.0.44:5000/jenkins_springboot
+
+
+aliimage=$image_name:`TZ=CST-8 date '+%Y%m%d-%H%M'`
+echo $aliimage
+
+if [ "`docker images | awk '/^10.0.0.44:5000\/jenkins_springboot/ { print $3 }'`" ]; then
+    docker rmi -f $(docker images | awk '/^10.0.0.44:5000\/jenkins_springboot/ { print $3 }' );
+fi
 mvn clean install -U  -Dmaven.test.skip=true
 docker build --no-cache -t $aliimage .
 echo "Push Image:" $aliimage " to repository..."
